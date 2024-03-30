fbe7a266c3271f89968d8dfac7c99841ad8d8093
C Wael <wael.charni@outlook.fr>
2024-03-30 22:21:04 +0100

    Added 2 new functions, getMean, and GetMedian, I wrote a comment because I did an implicit return for this function.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,13 @@
-const getMean = (array) => {
-    const sum = array.reduce();
+/** const getMean = (array) => {
+    const sum = array.reduce((acc, el) => acc + el, 0); // reduce va itérer sur chaque élément du tableau, va ajouter chaque élément qui s'ajouteront à chaque fois sur acc pour donner au final le résultat de l'addition de chaque élément du tableau.
+    const mean = sum / array.length;
+    return mean;
+}*/
+
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+    const sorted = array.sort((a, b) => a - b);
 }
 
 const calculate = () => {
