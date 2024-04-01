a8a747673d13bb91038f665bd0468dfda678d55a
C Wael <wael.charni@outlook.fr>
2024-04-01 22:57:24 +0200

    chain on the calculate function.

--- script/script.js
+++ script/script.js
@@ -13,5 +13,6 @@ const getMedian = (array) => {
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
-    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el))
+    .filter(el => !isNaN(el));
 }
