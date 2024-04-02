8492f9e443bd37e2fea6d0db3c71f38ea8797fea
C Wael <wael.charni@outlook.fr>
2024-04-02 21:57:56 +0200

    Adding comment to each line of code that I don't understand

--- script/script.js
+++ script/script.js
@@ -15,17 +15,19 @@ const getMedian = (array) => {
     return median;
 }
 
+// The mode is the number that appears most often is the list.
 const getMode = (array) => {
-    const counts = {};
-    array.forEach((el) => {
-        counts[el] = (counts[el] || 0) + 1;
+    const counts = {}; // Used to track the number of times each number appears in the list
+    array.forEach((el) => { // looping through the array.
+        counts[el] = (counts[el] || 0) + 1; // accessing the counts object and incrementing the count for each number.
     })
-    if (new Set(Object.values(counts)).size === 1) {
-        return null;
+    if (new Set(Object.values(counts)).size === 1) { // set object lets you store unique value of any type, Object.values returns an array of a given object, and size displays the number of unique values present in the set object.
+        return null; // so if there is only one unique value present its gonna return null
     }
-    const highest = Object.keys(counts).sort(
-        (a, b) => counts[b] - counts[a]
-      )[0];
+    const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0]; // sorting the keys of the counts array in descending order, and retrieving the first key from the sorted array
+    const mode = Object.keys(counts);
+    
+    
 }
 
 const calculate = () => {
